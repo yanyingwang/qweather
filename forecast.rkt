4a13161f07f3fea27274c67f76ceeee3853244f4
Yanying Wang <yanyingwang1@gmail.com>
2021-02-28 00:29:51 +0800

    fix forecast procedures bugs

--- forecast.rkt
+++ forecast.rkt
@@ -10,56 +10,53 @@
                      #:lang [lang (current-qweather-lang)]
                      #:unit [unit "m"])
   (http-get (api-qweather)
-            #:path "/weather/now"
+            #:path "weather/now"
             #:data (hasheq 'location location
                            'gzip gzip
                            'lang lang
                            'unit unit)))
 
+
 (define (weather/3d location
-                    #:gzip [gzip (current-qweather-gzip)]
+                     #:gzip [gzip (current-qweather-gzip)]
                      #:lang [lang (current-qweather-lang)]
                      #:unit [unit "m"])
   (http-get (api-qweather)
-            #:path "/weather/now"
-            #:data (weather/3d 'location location
+            #:path "weather/3d"
+            #:data (hasheq 'location location
                            'gzip gzip
                            'lang lang
                            'unit unit)))
-
 (define (weather/7d location
-                    #:gzip [gzip (current-qweather-gzip)]
+                     #:gzip [gzip (current-qweather-gzip)]
                      #:lang [lang (current-qweather-lang)]
                      #:unit [unit "m"])
   (http-get (api-qweather)
-            #:path "/weather/now"
-            #:data (weather/7d 'location location
+            #:path "weather/7d"
+            #:data (hasheq 'location location
                            'gzip gzip
                            'lang lang
                            'unit unit)))
-
 (define (weather/10d location
                      #:gzip [gzip (current-qweather-gzip)]
                      #:lang [lang (current-qweather-lang)]
                      #:unit [unit "m"])
   (http-get (api-qweather)
-            #:path "/weather/now"
-            #:data (weather/10d 'location location
+            #:path "weather/10d"
+            #:data (hasheq 'location location
                            'gzip gzip
                            'lang lang
                            'unit unit)))
-
 (define (weather/15d location
                      #:gzip [gzip (current-qweather-gzip)]
                      #:lang [lang (current-qweather-lang)]
                      #:unit [unit "m"])
   (http-get (api-qweather)
-            #:path "/weather/now"
-            #:data (weather/15d 'location location
+            #:path "weather/15d"
+            #:data (hasheq 'location location
                            'gzip gzip
                            'lang lang
                            'unit unit)))
-
 (define (weather/24h location
                      #:gzip [gzip (current-qweather-gzip)]
                      #:lang [lang (current-qweather-lang)]
@@ -70,7 +67,6 @@
                            'gzip gzip
                            'lang lang
                            'unit unit)))
-
 (define (weather/72h location
                      #:gzip [gzip (current-qweather-gzip)]
                      #:lang [lang (current-qweather-lang)]
@@ -81,9 +77,8 @@
                            'gzip gzip
                            'lang lang
                            'unit unit)))
-
 (define (weather/168h location
-                      #:gzip [gzip (current-qweather-gzip)]
+                     #:gzip [gzip (current-qweather-gzip)]
                      #:lang [lang (current-qweather-lang)]
                      #:unit [unit "m"])
   (http-get (api-qweather)
